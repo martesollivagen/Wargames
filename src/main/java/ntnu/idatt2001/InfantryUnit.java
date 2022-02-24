e204bbe8594f8b63ff3cd40ef3bd1a971f742a88
Marte Solli Vågen <martvaag@stud.ntnu.no>
2022-02-24 18:26:05 +0100

    Created class InfantryUnit, with javadoc

---
+++ src/main/java/ntnu/idatt2001/InfantryUnit.java
@@ -0,0 +1,35 @@
+package ntnu.idatt2001;
+
+/**
+ * class which represent a unit which specialises in melee (nærkamp)
+ * @version 1.0 2022-02-24
+ * @author martvaag
+ */
+public class InfantryUnit extends Unit{
+    /**
+     * simplified constructor with attack = 15 and armor = 10
+     * @param name name of unit
+     * @param health health-value of unit
+     */
+    public InfantryUnit(String name, int health){
+        super(name, health,15,10);
+    }
+
+    /**
+     * bonus added for melee-attack
+     * @return bonus = 2
+     */
+    @Override
+    public int getAttackbonus() {
+        return 2;
+    }
+
+    /**
+     * bonus added for defence
+     * @return bonus = 1
+     */
+    @Override
+    public int getResistBonus() {
+        return 1;
+    }
+}
