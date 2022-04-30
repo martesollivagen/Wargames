f3a65db9df2964b8da2cbae42be5d3b2c995107b
Marte Solli Vågen <martvaag@stud.ntnu.no>
2022-04-30 14:25:15 +0200

    Removed file handling methods in Army class

--- src/main/java/ntnu/idatt2001/martvaag/Army.java
+++ src/main/java/ntnu/idatt2001/martvaag/Army.java
@@ -174,43 +174,4 @@ public class Army {
                 .filter(unitEqualsCommanderUnits)
                 .collect(Collectors.toList());
     }
-
-    /**
-     * write an army to a file
-     * @param file file
-     * @param army army
-     */
-    public static void writeToFile(File file, Army army){
-
-        try (FileWriter fileWriter = new FileWriter(file)){
-            fileWriter.write(army.getName());
-            for (Unit unit : army.getUnits()){
-                fileWriter.write("\n" + unit.toString());
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * read an Army from a file
-     * @param path file path
-     * @return Army from file
-     */
-    public static Army readArmyFromFile(String path){
-        Army army = null;
-        ArrayList<Unit> units = new ArrayList<>();
-        File fileToRead = new File(path);
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileToRead))){
-            String line = reader.readLine();
-            army = new Army(line, units);
-                while ((line = reader.readLine()) != null){
-                    String [] list = line.split(",");
-                    army.add(UnitFactory.createUnit(list[0], list[1], Integer.parseInt(list[2])));
-                }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-        return army;
-    }
 }
