ef851a5752969d80d4cb399a813f2e1f1dd945b8
Marte Solli Vågen <martvaag@stud.ntnu.no>
2022-05-18 13:43:15 +0200

    Added method to copy a unit

--- src/main/java/ntnu/idatt2001/martvaag/tools/factory/UnitFactory.java
+++ src/main/java/ntnu/idatt2001/martvaag/tools/factory/UnitFactory.java
@@ -1,14 +1,19 @@
-package ntnu.idatt2001.martvaag;
+package ntnu.idatt2001.martvaag.tools.factory;
 
+import ntnu.idatt2001.martvaag.tools.enums.UnitTypes;
 import ntnu.idatt2001.martvaag.unit.*;
 import java.util.ArrayList;
 
 /**
  * class to create new units
+ * @version 2022-05-18
+ * @author martvaag
  */
 public class UnitFactory {
+
     /**
      * create a new unit based on type, name and health
+     *
      * @param type type of unit
      * @param name name of unit
      * @param health unit's health
@@ -25,13 +30,16 @@ public class UnitFactory {
                 return new CavalryUnit(name, health);
             case COMMANDERUNIT:
                 return new CommanderUnit(name, health);
+            case SUPPORTUNIT:
+                return new SupportUnit(name, health);
             default:
                 return null;
         }
     }
 
     /**
-     * create multiple units based on type, name and healh
+     * create multiple units based on type, name and health
+     *
      * @param numberOfUnits number of units wanted to be created
      * @param type type of unit
      * @param name name of unit
@@ -45,5 +53,15 @@ public class UnitFactory {
         }
 
         return units;
+    }
+
+    /**
+     * create a copy of a unit
+     *
+     * @param unit unit
+     * @return copy of unit
+     */
+    public static Unit createUnitCopy(Unit unit){
+        return createUnit(UnitTypes.valueOf(unit.getClass().getSimpleName().toUpperCase()), unit.getName(), unit.getHealth());
     }
 }
