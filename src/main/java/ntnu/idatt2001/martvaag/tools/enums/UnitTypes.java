c1c326770114b7673dc293c19059da63b58a4c93
Marte Solli Vågen <martvaag@stud.ntnu.no>
2022-05-18 14:00:36 +0200

    Added SupportUnit as enum in UnitTypes

--- src/main/java/ntnu/idatt2001/martvaag/tools/enums/UnitTypes.java
+++ src/main/java/ntnu/idatt2001/martvaag/tools/enums/UnitTypes.java
@@ -1,9 +1,9 @@
-package ntnu.idatt2001.martvaag;
+package ntnu.idatt2001.martvaag.tools.enums;
 
 /**
  * enum for all the different valid unit types
  * with an attached String value of the unit type
- * @version 2022-05-10
+ * @version 2022-05-18
  * @author martvaag
  */
 public enum UnitTypes {
@@ -11,11 +11,9 @@ public enum UnitTypes {
     INFANTRYUNIT("infantryunit"),
     CAVALRYUNIT("cavalryunit"),
     RANGEDUNIT("rangedunit"),
-    COMMANDERUNIT("commanderunit");
-
-    private final String unitType;
+    COMMANDERUNIT("commanderunit"),
+    SUPPORTUNIT("supportunit");
 
     UnitTypes(String unitName){
-        this.unitType = unitName;
     }
 }
