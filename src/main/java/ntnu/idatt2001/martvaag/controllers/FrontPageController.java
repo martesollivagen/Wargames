c5b9844ed8e8c162ef32f476b9d7cfe9e313b341
Marte Solli Vågen <martvaag@stud.ntnu.no>
2022-05-18 15:32:51 +0200

    Moved controller to new package

--- src/main/java/ntnu/idatt2001/martvaag/controllers/FrontPageController.java
+++ src/main/java/ntnu/idatt2001/martvaag/controllers/FrontPageController.java
@@ -1,4 +1,4 @@
-package ntnu.idatt2001.martvaag;
+package ntnu.idatt2001.martvaag.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
@@ -10,10 +10,21 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
+/**
+ * controller for the front page of the application
+ * @version 1.0 2022-05-18
+ * @author martvaag
+ */
 public class FrontPageController {
 
+    /**
+     * opens the next page of the application
+     * @param event event
+     * @throws IOException IOException
+     */
     public void createAnArmy(ActionEvent event) throws IOException {
-        Parent tableViewParent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("CreateArmy.fxml")));
+        String createArmyFilePath = "/ntnu/idatt2001/martvaag/fxmlFiles/CreateArmy.fxml";
+        Parent tableViewParent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(createArmyFilePath)));
         Scene tableViewScene = new Scene(tableViewParent);
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(tableViewScene);
