18f399a7a1a5ea45da3b78001e67d9dea02717cc
Marte Solli Vågen <martvaag@stud.ntnu.no>
2022-05-18 15:29:58 +0200

    Minor change to code

--- src/main/java/ntnu/idatt2001/martvaag/WargamesApplication.java
+++ src/main/java/ntnu/idatt2001/martvaag/WargamesApplication.java
@@ -11,19 +11,28 @@ import java.util.Objects;
 /**
  * main class that extends Application
  * used to start and open the application
- * @version 2022-05-10
+ * @version 2022-05-18
  * @author martvaag
  */
 public class WargamesApplication extends Application {
-
+    /**
+     * method to start the application which opens the front page
+     * @param primaryStage primary stage
+     * @throws Exception exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("FrontPage.fxml")));
+        String frontPageFilePath = "/ntnu/idatt2001/martvaag/fxmlFiles/FrontPage.fxml";
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(frontPageFilePath)));
         primaryStage.setTitle("Wargames");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
+    /**
+     * main-method
+     * @param args Srgs
+     */
     public static void main(String[] args) {
         launch(args);
     }
