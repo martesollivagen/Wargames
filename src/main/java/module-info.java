f7f4300264a097ca573d068d896eef711a24d918
Marte Solli Vågen <martvaag@stud.ntnu.no>
2022-05-18 14:24:21 +0200

    Updated module-info file because of new packages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,19 @@ module Wargames{
     requires javafx.controls;
     requires javafx.fxml;
     exports ntnu.idatt2001.martvaag;
+    exports ntnu.idatt2001.martvaag.unit;
     opens ntnu.idatt2001.martvaag to javafx.fxml;
+    opens ntnu.idatt2001.martvaag.unit to javafx.fxml;
+    exports ntnu.idatt2001.martvaag.controllers;
+    opens ntnu.idatt2001.martvaag.controllers to javafx.fxml;
+    exports ntnu.idatt2001.martvaag.tools.observer;
+    opens ntnu.idatt2001.martvaag.tools.observer to javafx.fxml;
+    exports ntnu.idatt2001.martvaag.tools.enums;
+    opens ntnu.idatt2001.martvaag.tools.enums to javafx.fxml;
+    exports ntnu.idatt2001.martvaag.tools.filehandling;
+    opens ntnu.idatt2001.martvaag.tools.filehandling to javafx.fxml;
+    exports ntnu.idatt2001.martvaag.tools.factory;
+    opens ntnu.idatt2001.martvaag.tools.factory to javafx.fxml;
+    exports ntnu.idatt2001.martvaag.battle;
+    opens ntnu.idatt2001.martvaag.battle to javafx.fxml;
 }
