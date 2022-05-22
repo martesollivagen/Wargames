5c6f86ac035e5bd57382dca6ec2e6a693b45b252
Marte Solli Vågen <martvaag@stud.ntnu.no>
2022-05-22 13:38:18 +0200

    Created own method for fight to use in simulate

--- src/main/java/ntnu/idatt2001/martvaag/battle/Battle.java
+++ src/main/java/ntnu/idatt2001/martvaag/battle/Battle.java
@@ -9,24 +9,24 @@ import java.util.Locale;
 
 /**
  * class which represent a battle between two armies
- * @version 2022-05-18
+ * @version 2022-05-22
  * @author  martvaag
  */
 public class Battle extends Subject {
-    private final Army HumanArmy;
-    private final Army OrcishHorde;
+    private final Army armyOne, armyTwo;
     private Army winner;
     private Terrain terrainOfBattle;
 
     /**
      * constructor to a battle between two armies
-     * @param HumanArmy   Army 1
-     * @param OrcishHorde Army 2
+     *
+     * @param armyOne Army one
+     * @param armyTwo Army two
      */
-    public Battle(Army HumanArmy, Army OrcishHorde) {
-        if (HumanArmy == null || OrcishHorde == null) throw new IllegalArgumentException("The armies cannot be null");
-        this.HumanArmy = HumanArmy;
-        this.OrcishHorde = OrcishHorde;
+    public Battle(Army armyOne, Army armyTwo) {
+        if (armyOne == null || armyTwo == null) throw new IllegalArgumentException("The armies cannot be null");
+        this.armyOne = armyOne;
+        this.armyTwo = armyTwo;
     }
 
     /**
@@ -35,6 +35,22 @@ public class Battle extends Subject {
      */
     public Army getWinner() {
         return winner;
+    }
+
+    /**
+     * get army one
+     * @return army one
+     */
+    public Army armyOne(){
+        return armyOne;
+    }
+
+    /**
+     * get army two
+     * @return army two
+     */
+    public Army armyTwo(){
+        return armyTwo;
     }
 
     /**
@@ -47,10 +63,9 @@ public class Battle extends Subject {
 
     /**
      * simulate a battle between armyOne and armyTwo
-     * picks a random unit from one army to attack a random unit from the other army
-     * if a unit has a health-value equal to 0, it is removed from the army and observes will get notified on the army and which unit died
-     * if the unit that is removed from the army is a SupportUnit, it will heal a random unit from its army
-     * this process continues until one army has lost all it´s units and the winning army is returned
+     * uses the fight method, and returns the winner of the battle
+     *
+     * @param terrain terrain
      * @return the winning army
      */
     public Army simulate(Terrain terrain){
@@ -60,39 +75,51 @@ public class Battle extends Subject {
             case HILL:
             case FOREST:
             case PLAINS:
-                int attack = 1;
-                while (HumanArmy.hasUnits() && OrcishHorde.hasUnits()){
-                    Unit humanUnit = HumanArmy.getRandom();
-                    Unit OrcishUnit = OrcishHorde.getRandom();
-                    if (attack % 2 == 0){
-                        HumanArmy.getRandom().attack(OrcishUnit, terrain);
-                        if (OrcishUnit.isDead()){
-                            if (OrcishUnit instanceof SupportUnit){
-                                OrcishUnit.heal(OrcishHorde.getRandom());
-                            }
-                            OrcishHorde.remove(OrcishUnit);
-                            notifyObservers(OrcishHorde,OrcishUnit);
-                        }
-                    } else {
-                        OrcishHorde.getRandom().attack(humanUnit, terrain);
-                        if (humanUnit.isDead()){
-                            if (humanUnit instanceof SupportUnit){
-                                humanUnit.heal(HumanArmy.getRandom());
-                            }
-                            HumanArmy.remove(humanUnit);
-                            notifyObservers(HumanArmy,humanUnit);
-                        }
-                    }
-                    attack++;
-                }
-                if (!HumanArmy.hasUnits()){
-                    winner = OrcishHorde;
+                fight(terrain);
+                if (!armyOne.hasUnits()){
+                    winner = armyTwo;
                 } else {
-                    winner = HumanArmy;
+                    winner = armyOne;
                 }
                 return winner;
             default:
-                throw new IllegalArgumentException("Invalid terrain");
+                return null;
+        }
+    }
+
+    /**
+     * picks a random unit from one army to attack a random unit from the other army
+     * if a unit has a health-value equal to 0, it is removed from the army and observes will get notified on the army and which unit died
+     * if the unit that is removed from the army is a SupportUnit, it will heal a random unit from its own army
+     * this process continues until one army has lost all it´s units
+     *
+     * @param terrain terrain
+     */
+    public void fight(Terrain terrain){
+        int attack = 1;
+        while (armyOne.hasUnits() && armyTwo.hasUnits()){
+            Unit armyOneUnit = armyOne.getRandom();
+            Unit armyTwoUnit = armyTwo.getRandom();
+            if (attack % 2 == 0){
+                armyOne.getRandom().attack(armyTwoUnit, terrain);
+                if (armyTwoUnit.isDead()){
+                    if (armyTwoUnit instanceof SupportUnit){
+                        armyTwoUnit.heal(armyTwo.getRandom());
+                    }
+                    armyTwo.remove(armyTwoUnit);
+                    notifyObservers(armyTwo,armyTwoUnit);
+                }
+            } else {
+                armyTwo.getRandom().attack(armyOneUnit, terrain);
+                if (armyOneUnit.isDead()){
+                    if (armyOneUnit instanceof SupportUnit){
+                        armyOneUnit.heal(armyOne.getRandom());
+                    }
+                    armyOne.remove(armyOneUnit);
+                    notifyObservers(armyOne,armyOneUnit);
+                }
+            }
+            attack++;
         }
     }
 
@@ -101,6 +128,6 @@ public class Battle extends Subject {
      * @return textual representation of a battle
      */
     public String toString(){
-        return "The winning army of this " + getTerrainOfBattle() + " battle is: " + winner;
+        return "The winning army of this " + getTerrainOfBattle() + " battle is:\n" + getWinner();
     }
 }
