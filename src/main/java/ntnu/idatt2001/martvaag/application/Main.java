db6ebe0fb235f7da917d9468afa4273f7d7b46d3
Marte Solli Vågen <martvaag@stud.ntnu.no>
2022-05-22 13:01:44 +0200

    New main class to start the application

---
+++ src/main/java/ntnu/idatt2001/martvaag/application/Main.java
@@ -0,0 +1,17 @@
+package ntnu.idatt2001.martvaag.application;
+
+/**
+ * class to start the application that does not inherit from the javaFX class Application
+ * @version 2022-05-22
+ * @author martvaag
+ */
+public class Main {
+
+    /**
+     * main-method that uses the WargamesApplication class to open the application
+     * @param args Args
+     */
+    public static void main(String[] args) {
+        WargamesApplication.main(args);
+    }
+}
