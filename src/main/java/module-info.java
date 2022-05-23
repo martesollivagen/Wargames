9c64f22d55db6d94cb7fa7697ec566f76b087554
Marte Solli Vågen <martvaag@stud.ntnu.no>
2022-05-23 21:34:28 +0200

    Updated module-info file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,20 +2,22 @@ module Wargames{
     requires javafx.controls;
     requires javafx.fxml;
 
-    exports ntnu.idatt2001.martvaag.unit;
-    opens ntnu.idatt2001.martvaag.unit to javafx.fxml;
-    exports ntnu.idatt2001.martvaag.controllers;
-    opens ntnu.idatt2001.martvaag.controllers to javafx.fxml;
-    exports ntnu.idatt2001.martvaag.tools.observer;
-    opens ntnu.idatt2001.martvaag.tools.observer to javafx.fxml;
-    exports ntnu.idatt2001.martvaag.tools.enums;
-    opens ntnu.idatt2001.martvaag.tools.enums to javafx.fxml;
-    exports ntnu.idatt2001.martvaag.tools.filehandling;
-    opens ntnu.idatt2001.martvaag.tools.filehandling to javafx.fxml;
-    exports ntnu.idatt2001.martvaag.tools.factory;
-    opens ntnu.idatt2001.martvaag.tools.factory to javafx.fxml;
-    exports ntnu.idatt2001.martvaag.battle;
-    opens ntnu.idatt2001.martvaag.battle to javafx.fxml;
-    exports ntnu.idatt2001.martvaag.application;
-    opens ntnu.idatt2001.martvaag.application to javafx.fxml;
+    exports ntnu.idatt2001.martvaag.controller;
+    opens ntnu.idatt2001.martvaag.controller to javafx.fxml;
+    exports ntnu.idatt2001.martvaag.view;
+    opens ntnu.idatt2001.martvaag.view to javafx.fxml;
+    exports ntnu.idatt2001.martvaag.model.battle;
+    opens ntnu.idatt2001.martvaag.model.battle to javafx.fxml;
+    exports ntnu.idatt2001.martvaag.model.animation;
+    opens ntnu.idatt2001.martvaag.model.animation to javafx.fxml;
+    exports ntnu.idatt2001.martvaag.model.unit;
+    opens ntnu.idatt2001.martvaag.model.unit to javafx.fxml;
+    exports ntnu.idatt2001.martvaag.model.tools.filehandling;
+    opens ntnu.idatt2001.martvaag.model.tools.filehandling to javafx.fxml;
+    exports ntnu.idatt2001.martvaag.model.tools.enums;
+    opens ntnu.idatt2001.martvaag.model.tools.enums to javafx.fxml;
+    exports ntnu.idatt2001.martvaag.model.tools.factory;
+    opens ntnu.idatt2001.martvaag.model.tools.factory to javafx.fxml;
+    exports ntnu.idatt2001.martvaag.model.tools.observer;
+    opens ntnu.idatt2001.martvaag.model.tools.observer to javafx.fxml;
 }
